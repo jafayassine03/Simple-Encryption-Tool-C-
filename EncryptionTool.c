9d3e9ba7e5a1011bd3b400bb2def1b422410e600
Jafa Yassine <jafayassine03@gmail.com>
2026-03-14 17:08:26 +0800

    Add files via upload

--- EncryptionTool.c
+++ EncryptionTool.c
@@ -4,21 +4,38 @@
 int main() {
     char message[200];
     int key;
+    int choice;
     int i;
 
-    printf(" Simple Encryption Tool\n");
+    printf("=== Simple Encryption Tool ===\n");
+    printf("1. Encrypt Message\n");
+    printf("2. Decrypt Message\n");
+
+    printf("Choose an option: ");
+    scanf("%d", &choice);
+    getchar(); 
 
     printf("Enter a message: ");
     fgets(message, sizeof(message), stdin);
 
-    printf("Enter the encryption key (number): ");
+    printf("Enter the key (number): ");
     scanf("%d", &key);
 
-    for(i = 0; i < strlen(message); i++) {
-        message[i] = message[i] + key;
+    if(choice == 1) {
+        for(i = 0; i < strlen(message); i++) {
+            message[i] = message[i] + key;
+        }
+        printf("\nEncrypted message: %s", message);
     }
-
-    printf("\nEncrypted message: %s", message);
+    else if(choice == 2) {
+        for(i = 0; i < strlen(message); i++) {
+            message[i] = message[i] - key;
+        }
+        printf("\nDecrypted message: %s", message);
+    }
+    else {
+        printf("Invalid choice.\n");
+    }
 
     return 0;
 }
