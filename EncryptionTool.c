bd65c253e7c9cdd006b8147e858af4d4301dcdc6
Jafa Yassine <jafayassine03@gmail.com>
2026-03-16 17:10:54 +0800

    Add files via upload

--- EncryptionTool.c
+++ EncryptionTool.c
@@ -7,55 +7,68 @@ int main() {
     int choice;
     int i;
     char save;
+    char filename[100];
     FILE *file;
+    char again;
 
-    printf("=== Simple Encryption Tool ===\n");
-    printf("1. Encrypt Message\n");
-    printf("2. Decrypt Message\n");
+    do {
+        printf("=== Simple Encryption Tool ===\n");
+        printf("1. Encrypt Message\n");
+        printf("2. Decrypt Message\n");
 
-    printf("Choose an option: ");
-    scanf("%d", &choice);
-    getchar();
+        printf("Choose an option: ");
+        scanf("%d", &choice);
+        getchar(); 
+        printf("Enter a message: ");
+        fgets(message, sizeof(message), stdin);
 
-    printf("Enter a message: ");
-    fgets(message, sizeof(message), stdin);
+        printf("Enter the key (number): ");
+        scanf("%d", &key);
 
-    printf("Enter the key (number): ");
-    scanf("%d", &key);
-
-    if(choice == 1) {
-        for(i = 0; i < strlen(message); i++) {
-            message[i] = message[i] + key;
+        if(choice == 1) {
+            for(i = 0; i < strlen(message); i++) {
+                message[i] = message[i] + key;
+            }
+            printf("\nEncrypted message: %s", message);
         }
-        printf("\nEncrypted message: %s", message);
-    }
-    else if(choice == 2) {
-        for(i = 0; i < strlen(message); i++) {
-            message[i] = message[i] - key;
+        else if(choice == 2) {
+            for(i = 0; i < strlen(message); i++) {
+                message[i] = message[i] - key;
+            }
+            printf("\nDecrypted message: %s", message);
         }
-        printf("\nDecrypted message: %s", message);
-    }
-    else {
-        printf("Invalid choice.\n");
-        return 0;
-    }
-
-    printf("\nDo you want to save the result to a file? (y/n): ");
-    scanf(" %c", &save);
-
-    if(save == 'y' || save == 'Y') {
-        file = fopen("result.txt", "w");
-
-        if(file == NULL) {
-            printf("Error opening file.\n");
-            return 1;
+        else {
+            printf("Invalid choice.\n");
+            continue;
         }
 
-        fprintf(file, "%s", message);
-        fclose(file);
+        printf("\nDo you want to save the result to a file? (y/n): ");
+        scanf(" %c", &save);
 
-        printf("Message saved to result.txt\n");
-    }
+        if(save == 'y' || save == 'Y') {
+            printf("Enter the filename to save (with .txt): ");
+            scanf("%s", filename);
 
+            file = fopen(filename, "w");
+
+            if(file == NULL) {
+                printf("Error opening file.\n");
+                return 1;
+            }
+
+            fprintf(file, "%s", message);
+            fclose(file);
+
+            printf("Message saved to %s\n", filename);
+        }
+
+        printf("\nDo you want to process another message? (y/n): ");
+        scanf(" %c", &again);
+        getchar(); // consume newline
+        printf("\n");
+
+    } while(again == 'y' || again == 'Y');
+
+    printf("Goodbye!\n");
     return 0;
 }
